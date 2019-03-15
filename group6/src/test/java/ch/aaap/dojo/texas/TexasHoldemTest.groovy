54648cc1cb7416d9d16d3cc66e07fc0e628a012f
Olivera Mitrović <olivera@3ap.ch>
2019-03-15 14:57:14 +0000

    no message

--- group6/src/test/java/ch/aaap/dojo/texas/TexasHoldemTest.groovy
+++ group6/src/test/java/ch/aaap/dojo/texas/TexasHoldemTest.groovy
@@ -4,5 +4,14 @@ import spock.lang.Specification
 
 class TexasHoldemTest extends Specification {
     
-    
+    void "return highest card"() {
+    	given: 
+    		List<String> cards = ["Kc", "9s", "Ks", "Kd", "9d", "3c", "6d"];
+    		String exptectedResult = "Kc";
+    	when:
+    	
+    	then: 
+    		exptectedResult 
+    	
+    }
 }
