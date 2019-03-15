d98fc22e8a33ced11c4ddcf35d292b98d9d473c7
rajicdalibor <rajicdalibor@yahoo.com>
2019-03-15 15:15:52 +0000

    Added git ignore

--- group1/Card.ts
+++
@@ -1,6 +0,0 @@
-import { CardSuit } from './CardSuit';
-import { CardValue } from './CardValue';
-
-export class Card {
-    public constructor(private value: CardValue, private suit: CardSuit) {}
-}
--- group1/CardSuit.ts
+++
@@ -1,6 +0,0 @@
-export const enum CardSuit {
-    HEARTS = 'Hearts',
-    DIAMONDS = 'Diamonds',
-    SPADES = 'Spades',
-    CLUBS = 'Clubs'
-}
