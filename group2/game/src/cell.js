981df6ee226fc4774997219426dad0994717279c
Veljko Tasic <veljko@3ap.ch>
2019-03-15 15:41:31 +0000

    styled button

--- group2/game/src/cell.js
+++ group2/game/src/cell.js
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function Cell({alive, onClick}) {
   return (
-    <button onClick={onClick}>{alive ? <span>*</span> : <span>-</span>}</button>
+    <button onClick={onClick}>{alive ? <span>*</span> : <span>-</span>}
+      {/* language=CSS */}
+      <style jsx>{`
+          button {
+              font-size: 14px;
+              border: none;
+              padding: 10px 10px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              cursor: pointer;
+              outline: none;
+              margin: 5px;
+          }
+      `}</style>
+    </button>
   );
 }
