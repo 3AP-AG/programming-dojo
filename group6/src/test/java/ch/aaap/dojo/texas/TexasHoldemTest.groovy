e0e3e7b22a181da9b3c80868ffabb308edcc7e39
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2019-03-15 16:58:34 +0000

    Refactor pair test

--- group6/src/test/java/ch/aaap/dojo/texas/TexasHoldemTest.groovy
+++ group6/src/test/java/ch/aaap/dojo/texas/TexasHoldemTest.groovy
@@ -1,6 +1,7 @@
 package ch.aaap.dojo.texas
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class TexasHoldemTest extends Specification {
 
@@ -24,10 +25,10 @@ class TexasHoldemTest extends Specification {
         given:
         List<List<String>> cardSets =
             [["Kc", "9s", "Ks", "Kd", "9d", "3c", "6d"],
-            ["9c", "Ah", "Ks", "Kd", "9d", "3c", "6d"],
+             ["9c", "Ah", "Ks", "Kd", "9d", "3c", "6d"],
              ["Ac", "Qc", "Ks", "Kd", "9d", "3c"]]
         List<List<String>> expectedResult = [["9c", "Ah", "Ks", "Kd", "9d", "3c", "6d"],
-                                       ["Ac", "Qc", "Ks", "Kd", "9d", "3c"]]
+                                             ["Ac", "Qc", "Ks", "Kd", "9d", "3c"]]
 
         when:
         List<List<String>> result = texasHoldem.extractHighestCardSets(cardSets)
@@ -35,13 +36,19 @@ class TexasHoldemTest extends Specification {
         expectedResult == result
     }
 
-    void "return pair"() {
-        given:
-        List<String> cardSets =
-            ["Kc", "9s", "Ks", "Kd", "9d", "3c", "6d"]
+    @Unroll
+    void "return is pair #expectPair for set #cardSets"() {
+
         when:
         boolean isPair = texasHoldem.hasPair(cardSets)
+
         then:
-        isPair
+        expectPair == isPair
+
+        where:
+        cardSets                                   | expectPair
+        ["Kc", "9s", "Ks", "Kd", "9d", "3c", "6d"] | true
+        ["Kd", "9d", "3c", "6d"]                   | false
     }
+
 }
