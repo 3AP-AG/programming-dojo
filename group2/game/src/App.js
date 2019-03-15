1225a4bc52dc3d485576ba510f5b072613a4d905
Veljko Tasic <veljko@3ap.ch>
2019-03-15 14:32:32 +0000

    work

--- group2/game/src/App.js
+++ group2/game/src/App.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    matrix: [[]],
+    dimensionX: 0,
+    dimensionY: 0,
+  };
+
+  inputX = (event) => {
+    console.log('EVENT:', event.target.value)
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          {/*<p>*/}
+            {/*Edit <code>src/App.js</code> and save to reload.*/}
+          {/*</p>*/}
+          {/*<a*/}
+            {/*className="App-link"*/}
+            {/*href="https://reactjs.org"*/}
+            {/*target="_blank"*/}
+            {/*rel="noopener noreferrer"*/}
+          {/*>*/}
+            {/*Learn React*/}
+          {/*</a>*/}
+
+          <input onChange={this.inputX}/>
+          Test
         </header>
       </div>
     );
