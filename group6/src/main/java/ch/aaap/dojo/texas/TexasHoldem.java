70eaa0529019a528789a731fc3f0882ed3841537
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2019-03-15 15:36:35 +0000

    First passing test

--- group6/src/main/java/ch/aaap/dojo/texas/TexasHoldem.java
+++ group6/src/main/java/ch/aaap/dojo/texas/TexasHoldem.java
@@ -1,4 +1,43 @@
 package ch.aaap.dojo.texas;
 
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Map.entry;
+
 public class TexasHoldem {
+
+    private static final Map<Character, Integer> CARD_RANK = Map.ofEntries(
+        entry('2', 2),
+        entry('3', 3),
+        entry('4', 4),
+        entry('5', 5),
+        entry('6', 6),
+        entry('7', 7),
+        entry('8', 8),
+        entry('9', 9),
+        entry('T', 10),
+        entry('J', 11),
+        entry('Q', 12),
+        entry('K', 13),
+        entry('A', 14)
+    );
+
+    public String extractHighestCard(List<String> cards) {
+        String maxCard = "";
+        int currentMax = 0;
+
+        for (final String card :
+            cards) {
+            char rankOfCard = card.charAt(0);
+            Integer rank = CARD_RANK.get(rankOfCard);
+
+            if(currentMax < rank) {
+                currentMax = rank;
+                maxCard = card;
+            }
+        }
+
+        return maxCard;
+    }
 }
--- group6/src/test/java/ch/aaap/dojo/texas/TexasHoldemTest.groovy
+++ group6/src/test/java/ch/aaap/dojo/texas/TexasHoldemTest.groovy
@@ -3,16 +3,19 @@ package ch.aaap.dojo.texas
 import spock.lang.Specification
 
 class TexasHoldemTest extends Specification {
-    
+
     void "return highest card"() {
-    	given: 
-    		List<String> cards = ["Kc", "9s", "Ks", "Kd", "9d", "3c", "6d"];
-    		String exptectedResult = "Kc";
-            TexasHoldem texasHoldem = new TexasHoldem();
-    	when:
-            String result = texasHoldem.extractHighestCard(cards);
-    	then: 
-    		exptectedResult == result
-    	
+
+        given:
+        List<String> cards = ["Kc", "9s", "Ks", "Kd", "9d", "3c", "6d"]
+        String exptectedResult = "Kc"
+        TexasHoldem texasHoldem = new TexasHoldem()
+
+        when:
+        String result = texasHoldem.extractHighestCard(cards)
+
+        then:
+        exptectedResult == result
+
     }
 }
