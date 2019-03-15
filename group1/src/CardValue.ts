6701f36823f926e00d8a2bdcc49269d1ecabab31
rajicdalibor <rajicdalibor@yahoo.com>
2019-03-15 15:54:05 +0000

    Value from character

--- group1/src/CardValue.ts
+++ group1/src/CardValue.ts
@@ -1,4 +1,6 @@
-export const enum CardValue {
+import {CardSuit} from "./CardSuit";
+
+export const enum CardValueEnum {
     ONE = 'ONE',
     TWO = 'TWO',
     THREE = 'THREE',
@@ -13,4 +15,30 @@ export const enum CardValue {
     QUEEN = 'QUEEN',
     KING = 'KING',
     ACE = 'ACE'
+}
+
+export class CardValue {
+    constructor(private cardValueEnum: CardValueEnum) {}
+
+    public static cardValueFromCharacter(character: string): CardValue {
+        const characterValueType: any = {
+            '1': CardValueEnum.ONE,
+            '2': CardValueEnum.TWO,
+            '3': CardValueEnum.THREE,
+            '4': CardValueEnum.FOUR,
+            '5': CardValueEnum.FIVE,
+            '6': CardValueEnum.SIX,
+            '7': CardValueEnum.SEVEN,
+            '8': CardValueEnum.EIGHT,
+            '9': CardValueEnum.NINE,
+            'T': CardValueEnum.TEN,
+            'J': CardValueEnum.JACK,
+            'Q': CardValueEnum.QUEEN,
+            'K': CardValueEnum.KING,
+            'A': CardValueEnum.ACE
+        };
+
+        return new CardValue(characterValueType[character]);
+    }
+
 }
--- group1/src/index.ts
+++ group1/src/index.ts
@@ -2,23 +2,6 @@ import { Card } from "./Card";
 import { CardSuit } from "./CardSuit";
 import { CardValue } from "./CardValue";
 
-const characterValueType: any = {
-    '1': CardValue.ONE,
-    '2': CardValue.TWO,
-    '3': CardValue.THREE,
-    '4': CardValue.FOUR,
-    '5': CardValue.FIVE,
-    '6': CardValue.SIX,
-    '7': CardValue.SEVEN,
-    '8': CardValue.EIGHT,
-    '9': CardValue.NINE,
-    'T': CardValue.TEN,
-    'J': CardValue.JACK,
-    'Q': CardValue.QUEEN,
-    'K': CardValue.KING,
-    'A': CardValue.ACE
-}
-
 function parseLine(line: string) {
     return line.split(' ');
 }
@@ -28,14 +11,9 @@ function cardFromString(cardString: string): Card{
     const rawValue = cardString[0];
 
     const suit = CardSuit.suitFromCharacter(rawSuit);
-    const value = valueFromCharacter(rawValue);
+    const value = CardValue.cardValueFromCharacter(rawValue);
     return new Card(value, suit);
 }
-
-function valueFromCharacter(rawValue: string): CardValue {
-    return characterValueType[rawValue];
-}
-
 
 const t = cardFromString('Kc');
 console.log(t);
