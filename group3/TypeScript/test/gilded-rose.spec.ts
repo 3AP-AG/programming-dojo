c222b58fee6a7d0903eab62a96e3f1edfae6a543
vladan <blea91@gmail.com>
2019-03-15 15:38:15 +0000

    Test implementation in progress.

--- group3/TypeScript/test/gilded-rose.spec.ts
+++ group3/TypeScript/test/gilded-rose.spec.ts
@@ -1,6 +1,21 @@
 import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
+const items = [
+    new Item("+5 Dexterity Vest", 10, 20), //
+    new Item("Aged Brie", 2, 0), //
+    new Item("Elixir of the Mongoose", 5, 7), //
+    new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+    new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+    // this conjured item does not work properly yet
+    new Item("Conjured Mana Cake", 3, 6)];
+
+
+const days: number = 2;
+
 describe('Gilded Rose', function () {
 
     it('should foo', function() {
@@ -8,5 +23,33 @@ describe('Gilded Rose', function () {
         const items = gildedRose.updateQuality();
         expect(items[0].name).to.equal('foo');
     });
+
+    it(`Asserting state after ${days} days`, function () {
+
+        const oldResult = {};
+        const gildedRose = new GildedRose(items);
+
+        for (let i = 0; i < days; i++) {
+            oldResult[i] = gildedRose.updateQuality();
+        }
+
+        const newResult = {};
+        const newGildedRose = new GildedRose(items);
+
+        for (let i = 0; i < days; i++) {
+            newResult[i] = newGildedRose.updateQualityDev();
+        }
+
+        for (let day = 0; day < days; day++) {
+            oldResult[day].forEach((item: Item, index: number) => {
+
+                console.log(item.sellIn)
+                console.log(newResult[day][index].sellIn)
+                expect(item.name).to.equal(newResult[day][index].name);
+                expect(item.sellIn).to.equal(newResult[day][index].sellIn);
+                expect(item.quality).to.equal(newResult[day][index].quality);
+            })
+        }
+    })
 
 });
