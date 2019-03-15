a331301833f9745188db4405ec21da328fcfdf4b
salle18 <salle18@gmail.com>
2019-03-15 16:10:52 +0000

    Add to string on card value.

--- group1/src/CardValue.ts
+++ group1/src/CardValue.ts
@@ -1,4 +1,5 @@
 import {CardSuit} from "./CardSuit";
+import { swapKeyValue } from "./utils";
 
 export const enum CardValueEnum {
     ONE = 'ONE',
@@ -17,32 +18,34 @@ export const enum CardValueEnum {
     ACE = 'ACE'
 }
 
+const characterValueType: any = {
+    '1': CardValueEnum.ONE,
+    '2': CardValueEnum.TWO,
+    '3': CardValueEnum.THREE,
+    '4': CardValueEnum.FOUR,
+    '5': CardValueEnum.FIVE,
+    '6': CardValueEnum.SIX,
+    '7': CardValueEnum.SEVEN,
+    '8': CardValueEnum.EIGHT,
+    '9': CardValueEnum.NINE,
+    'T': CardValueEnum.TEN,
+    'J': CardValueEnum.JACK,
+    'Q': CardValueEnum.QUEEN,
+    'K': CardValueEnum.KING,
+    'A': CardValueEnum.ACE
+};
+
+const valueCharacterType: any = swapKeyValue(characterValueType);
+
 export class CardValue {
     constructor(private cardValueEnum: CardValueEnum) {}
 
     public static cardValueFromCharacter(character: string): CardValue {
-        const characterValueType: any = {
-            '1': CardValueEnum.ONE,
-            '2': CardValueEnum.TWO,
-            '3': CardValueEnum.THREE,
-            '4': CardValueEnum.FOUR,
-            '5': CardValueEnum.FIVE,
-            '6': CardValueEnum.SIX,
-            '7': CardValueEnum.SEVEN,
-            '8': CardValueEnum.EIGHT,
-            '9': CardValueEnum.NINE,
-            'T': CardValueEnum.TEN,
-            'J': CardValueEnum.JACK,
-            'Q': CardValueEnum.QUEEN,
-            'K': CardValueEnum.KING,
-            'A': CardValueEnum.ACE
-        };
-
         return new CardValue(characterValueType[character]);
     }
 
     public toString():string{
-        return '';
+        return valueCharacterType[this.cardValueEnum];
     }
 
 }
